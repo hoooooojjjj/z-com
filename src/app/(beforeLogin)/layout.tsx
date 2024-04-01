8b0fec3043001c05485cdc9c86047b1e92d6238d
sono <rhj080471i@snu.ac.kr>
2024-04-01 23:03:55 +0900

    Feat : 패러렐 라우트

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import styles from "../page.module.css";
+export default function Layout({
+  children,
+  modal,
+}: {
+  children: ReactNode;
+  modal: ReactNode;
+}) {
+  return (
+    <div className={styles.container}>
+      비포 로그인 레이아웃
+      {children}
+      {/* 패러럴 모달을 통해 모달을 사용하려면 @modal과 사용할 파일이 같은 폴더 안에 있어야 한다 */}
+      {modal}
+    </div>
+  );
+}
