147a06262805dd81ae2c54f95ffe42b396faaedf
sono <rhj080471i@snu.ac.kr>
2024-04-01 17:20:10 +0900

    Feat : app router로 디렉토리 라우팅-> 주소에 맞는 디렉토리 생성
    git rebase --continue#

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,14 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
+  // 최상위 폴더
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      {/* 여기에 레이아웃(인터랙션이 있어도 안바뀌는 부분)을 넣어주면 됨 */}
       <body className={inter.className}>{children}</body>
     </html>
   );
