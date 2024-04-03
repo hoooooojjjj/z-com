6ad447920582b5178c83dc82f9900ae038a58a36
sono <rhj080471i@snu.ac.kr>
2024-04-03 17:57:59 +0900

    Feat : 인터샙팅 라우트

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -18,3 +18,6 @@ export default function Layout({
 // 주소가 localhost:3000일 때는 children->page.tsx, modal->@modal/default.tsx
 // 주소가 localhost:3000/i/flow/login 때는 chldren->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
 // => 패러럴 라우트 폴더 안에서도 여러 경로를 만들 수 있다.
+
+// 인터셉팅 라우트-(.)i/flow/login는 Link로 페이지가 이동했을 때만 실행되고,
+// 새로고침하거나 직접 주소창에 쳐서 들어가면 기존 i/flow/login 페이지가 라우팅된다.
