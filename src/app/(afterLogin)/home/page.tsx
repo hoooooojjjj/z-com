c80eae717dee1edf8c446aad32c76aa6f5f436b9
sono <rhj080471@gmail.com>
2024-04-08 21:00:13 +0900

    Feat : 홈탭 만들면서 Context API 적용해보기 -> 오류 발견!!

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,28 @@
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import style from "./home.module.css";
+// import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+// import Post from "@/app/(afterLogin)/_component/Post";
+
 export default function Home() {
-  return <main>홈페이지</main>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        {/* <PostForm />
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/> */}
+      </TabProvider>
+    </main>
+  );
 }
