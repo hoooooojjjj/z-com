51c941320f5a081b3307cc3b925c4698b01b9943
sono <rhj080471@gmail.com>
2024-04-15 21:26:41 +0900

    Feat : msw 세팅 /  msw 컴포넌트와 .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
+import { MSWComponent } from "./_component/MSWConponent";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       {/* 여기에 레이아웃(인터랙션이 있어도 안바뀌는 부분)을 넣어주면 됨 */}
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <MSWComponent />
+      </body>
     </html>
   );
 }
